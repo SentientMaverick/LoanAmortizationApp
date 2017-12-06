281a2f016c5676d7cc6331ed39239fe130a8d2a7
Tosin <monglord_13@yahoo.com>
2017-12-06 11:12:25 +0100

    Loan Amortization App first commit

---
+++ LoanAmortization/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoanAmortization.Models;
+
+namespace LoanAmortization.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult LoanApp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult LoanApp(SearchFormView loan)
+        {
+            List<LoanPaymentView> view = new List<LoanPaymentView>();
+            if (ModelState.IsValid)
+            {
+                view = GetLoanViewData(loan);
+            }
+            ViewBag.LoanData = view;
+            ViewBag.Principal = loan.LoanPrincipal;
+            ViewBag.Interest = view.Last().CummulativeInterestAmount;
+            ViewBag.Payment = view.Last().ScheduledPayment;
+            ViewBag.PaymentNumber = view.Last().PaymentNo;
+
+            return View();
+        }
+
+        public ViewResult LoanData(List<LoanPaymentView> LoanData)
+        {
+            List <LoanPaymentView> data= new List<LoanPaymentView>();
+            if (LoanData.Any())
+            {
+                data = LoanData;
+            }
+            return View(data);
+        }
+
+
+        private List<LoanPaymentView> GetLoanViewData(SearchFormView loan)
+        {
+            List<LoanPaymentView> listloan = new List<LoanPaymentView>();
+            int TotalNumberofPayments = loan.NoOfPaymentYears * loan.NoOfYearlyInstallmentalPayments;
+            double scheduledpayment = GetScheduledPayment(TotalNumberofPayments,loan.LoanPrincipal,loan.InterestRate);
+            double startingbalance; double cummulativeInterest=0;
+            DateTime lastpaymenttime=DateTime.Parse(loan.PaymentStartDate);
+            startingbalance = loan.LoanPrincipal;
+            
+            for (int i = 1; i <= TotalNumberofPayments; i++)
+            {
+                LoanPaymentView payment = new LoanPaymentView();
+                payment = GetPayment(startingbalance,loan.InterestRate, scheduledpayment);
+                payment.PaymentNo = i;
+                payment.ScheduledPayment = scheduledpayment;
+                cummulativeInterest += payment.InterestAmount;
+                payment.CummulativeInterestAmount = cummulativeInterest;
+                payment.paymentDate = lastpaymenttime.AddMonths(1).ToShortDateString();
+                startingbalance = payment.EndingBalance;
+                lastpaymenttime = DateTime.Parse(payment.paymentDate);
+                listloan.Add(payment);
+                 
+            }
+
+            return listloan;
+        }
+
+        private LoanPaymentView GetPayment(double startingbalance, double InterestRate, double scheduledpayment)
+        {
+            LoanPaymentView payment = new LoanPaymentView();
+           // payment.ScheduledPayment = scheduledpayment;
+            payment.BeginningBalance = startingbalance;
+            
+            double firstmul = 100 * 12;
+            double monthlyInterest = startingbalance * InterestRate / firstmul;
+            payment.InterestAmount = Math.Round(monthlyInterest,2);
+            payment.PrincipalAmount = Math.Round((scheduledpayment - monthlyInterest),2);
+            payment.EndingBalance = Math.Round(startingbalance - payment.PrincipalAmount, 2);
+            return payment;
+        }
+
+        private double GetScheduledPayment(int totalNumberofPayments, double loanPrincipal, double interestRate)
+        {
+            double firstmul = 100 * 12;
+            double intRate = interestRate / firstmul;
+            double monthly = (loanPrincipal * (Math.Pow((1 + intRate), totalNumberofPayments)) *
+                intRate / (Math.Pow((1 + intRate), totalNumberofPayments) - 1));
+            monthly= Math.Round(monthly, 2); ;
+            return monthly;
+        }
+    }
+}
---
+++ LoanAmortization/Models/AppModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoanAmortization.Models
+{
+    public class SearchFormView
+    {
+        [Key]
+        public int formid { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Loan Amount")]
+        public int LoanPrincipal { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Loan Period in Years")]
+        public int NoOfPaymentYears { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "No of Payments per year")]
+        public int NoOfYearlyInstallmentalPayments { get; set; }
+        [Required(ErrorMessage = "*")]
+        [Display(Name = "Annual Interest Rate")]
+        public int InterestRate{ get; set; }
+        [Display(Name = "Start Date Of Loan")]
+        public string PaymentStartDate { get; set; }
+    }
+
+    public class LoanPaymentView
+    {
+        [Key]
+        public int PaymentNo { get; set; }
+        [Display(Name = "Start Balance")]
+        public double BeginningBalance { get; set; }
+        public double ScheduledPayment { get; set; }
+        [Display(Name = "Principal")]
+        public double PrincipalAmount { get; set; }
+        [Display(Name = "End Balance")]
+        public double EndingBalance { get; set; }
+        [Display(Name = "Interest")]
+        public double InterestAmount { get; set; }
+
+        public string paymentDate { get; set; }
+
+        public double CummulativeInterestAmount { get; set; }
+    }
+
+}
